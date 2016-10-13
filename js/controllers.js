833498da66b46a6455d6bea4a0dc534be652f14b
Lachlan Grant <lachlangrant@rbvea.co>
2016-10-13 21:06:56 +1000

    JSON News

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,20 @@ pagesControllers
 
 .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {}])
 
-.controller('NewsCtrl', ['$scope', '$http', function($scope, $http) {}])
+.controller('NewsCtrl', ['$scope', '$http', function($scope, $http) {
+    var posts = [
+        {
+            title: "test post 2",
+            message: "msg 2"
+        },
+        {
+            title: "test 1",
+            message: "test msg"
+        }
+    ];
+
+    $scope.posts = posts;
+}])
 
 .controller('ContactCtrl', ['$scope', '$http', function($scope, $http) {}])
 
